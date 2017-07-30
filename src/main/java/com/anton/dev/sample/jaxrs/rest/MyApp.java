4f7f2d337b03a28336bdb5b2cce89ee85f35e86b
acrispin <antonio.crispin@gmail.com>
2017-07-29 19:33:12 -0500

    firts commit

---
+++ src/main/java/com/anton/dev/sample/jaxrs/rest/MyApp.java
@@ -0,0 +1,9 @@
+package com.anton.dev.sample.jaxrs.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class MyApp extends Application {
+	
+}
