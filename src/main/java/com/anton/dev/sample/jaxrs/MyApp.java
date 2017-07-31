03b4ad02d2272afe18d8372ccb1d5ee6326fffbf
acrispin <antonio.crispin@gmail.com>
2017-07-30 22:05:00 -0500

    update dependencies

--- src/main/java/com/anton/dev/sample/jaxrs/MyApp.java
+++ src/main/java/com/anton/dev/sample/jaxrs/MyApp.java
@@ -3,6 +3,11 @@ package com.anton.dev.sample.jaxrs;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ *
+ * @author anton
+ * @web https://javabrains.io/courses/javaee_advjaxrs
+ */
 @ApplicationPath("rest")
 public class MyApp extends Application {
     
